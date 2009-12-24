8295346ddc94b9b43cc42a57bb014b11028fefc5
Armin Häberling <armin.aha@gmail.com>
2009-12-24 15:43:16 +0100

    rename pdf output

--- createPdf.bat
+++ createPdf.bat
@@ -1,6 +1,6 @@
 set FOP_HOME="C:\Documents and Settings\Armin\Desktop\fop-0.95"
 
 cd fop
-call %FOP_HOME%\fop.bat -xml ..\dictionaries\MakingOutInKorean\making_out_in_korean.xml -xsl vocabularyCards.xsl -pdf ..\test2.pdf -c conf\fop.xconf
+call %FOP_HOME%\fop.bat -xml ..\dictionaries\MakingOutInKorean\making_out_in_korean.xml -xsl vocabularyCards.xsl -pdf ..\MakingOutInKorean.pdf -c conf\fop.xconf
 cd ..
 pause
