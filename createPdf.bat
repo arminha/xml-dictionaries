a4a0396cfea35642771bb77151731d01e92e5a71
Armin Häberling <armin.aha@gmail.com>
2010-01-03 16:01:40 +0100

    changed FOP_HOME
    TODO read FOP_HOME from environment variable

--- createPdf.bat
+++ createPdf.bat
@@ -1,4 +1,4 @@
-set FOP_HOME="C:\Documents and Settings\Armin\Desktop\fop-0.95"
+set FOP_HOME="D:\fop-0.95"
 
 cd fop
 call %FOP_HOME%\fop.bat -xml ..\dictionaries\MakingOutInKorean\making_out_in_korean.xml -xsl vocabularyCards.xsl -pdf ..\MakingOutInKorean.pdf -c conf\fop.xconf
