03302644e36b66a28403536d03020d74c3c4918c
Armin Häberling <armin.aha@gmail.com>
2011-01-15 14:42:12 +0100

    replace build scripts with ant

--- createPdf.bat
+++
@@ -1,6 +0,0 @@
-set FOP_HOME="D:\fop-0.95"
-
-cd fop
-call %FOP_HOME%\fop.bat -xml ..\dictionaries\MakingOutInKorean\making_out_in_korean.xml -xsl vocabularyCards.xsl -pdf ..\MakingOutInKorean.pdf -c conf\fop.xconf
-cd ..
-pause
--- createPdf.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-CURDIR=`pwd`
-cd $CURDIR/fop
-fop -xml ../dictionaries/MakingOutInKorean/making_out_in_korean.xml -xsl vocabularyCards.xsl -pdf ../MakingOutInKorean.pdf -c conf/fop.xconf
-cd $CURDIR
